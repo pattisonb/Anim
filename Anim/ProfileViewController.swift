ef43a69ea068f28c06564795beef57694165168e
pattisonb <Match815!>
2020-11-12 15:14:15 -0600

    adding prof pic

--- Anim/ProfileViewController.swift
+++ Anim/ProfileViewController.swift
@@ -9,5 +9,30 @@
 import UIKit
 class ProfileViewController: UIViewController {
     
+
+    
+    @IBOutlet weak var img: UIImageView!
+    var imagePicker : UIImagePickerController!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+    @IBAction func onClickPickImage(_ sender: Any) {
+        imagePicker = UIImagePickerController()
+        imagePicker.delegate = self;
+        imagePicker.sourceType = .photoLibrary
+        imagePicker.allowsEditing = true;
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
 }
-`
+
+extension ProfileViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        img.image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage
+        
+        dismiss(animated: true, completion: nil)
+    }
+}

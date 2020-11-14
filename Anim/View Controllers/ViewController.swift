5cf7fb5dad895c686303107e2548c40be99561db
maria <maria@Carolinas-MacBook-Air.local>
2020-11-14 09:43:05 -0600

    trying to get continue button to work without segue

--- Anim/View Controllers/ViewController.swift
+++ Anim/View Controllers/ViewController.swift
@@ -19,7 +19,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var errorLabel: UILabel!
     
     override func viewDidLoad() {
-        super.viewDidLoad()
+        super.viewDidLoad()  
 //         let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(UIInputViewController.dismissKeyboard))
 //
 //        tap.cancelsTouchesInView = false
